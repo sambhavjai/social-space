5f986de2a69466a68007f24b6c2230d2489ffba3
sambhavjai <38015768+sambhavjai@users.noreply.github.com>
2021-05-09 13:35:15 +0530

    added groups and posts apps

--- social_space/social_space/settings.py
+++ social_space/social_space/settings.py
@@ -121,6 +121,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIR = [BASE_DIR/'static']
+
+LOGIN_REDIRECT_URL = 'test'
+LOGOUT_REDIRECT_URL = 'thanks'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
