7059768b58e3f462f22a2a9fd8ecdf34ac94eea2
sambhavjai <38015768+sambhavjai@users.noreply.github.com>
2021-05-09 17:17:43 +0530

    added models

--- social_space/posts/models.py
+++ social_space/posts/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.conf import settings
+from django.urls import reverse
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+from groups.models import Group
+import misaka
 
 # Create your models here.
+class Post(models.Model):
+    user = models.ForeignKey(User,related_name='posts',on_delete=models.DO_NOTHING)
+    create_date = models.DateTimeField(auto_now=True)
+    message = models.TextField()
+    message_html = models.TextField(editable=False)
+    group = models.ForeignKey(Group,related_name='posts',null=True,blank=True,on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.message
+
+    def save(self,*args,**kwargs):
+        self.message_html = misaka.html(self.message)
+        super().save(*args,**kwargs)
+
+    def get_absolute_url(self):
+        return reverse('posts:single',kwargs={'username':self.user.username,'pk':self.pk})
+
+    class Meta():
+        ordering = ['-create_date']
+        unique_together = ('user','message')
